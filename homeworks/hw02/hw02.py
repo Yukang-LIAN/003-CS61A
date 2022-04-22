62d1e5fabcf0e9c3fe8b31102a94d5289296bc51
Badwoman <lianyukang@foxmail.com>
2022-04-22 23:03:56 +0800

    Finish HW02 pingpong

--- homeworks/hw02/hw02.py
+++ homeworks/hw02/hw02.py
@@ -1,117 +1,163 @@
-from operator import add, mul
-
-square = lambda x: x * x
-
-identity = lambda x: x
-
-triple = lambda x: 3 * x
-
-increment = lambda x: x + 1
+from operator import sub, mul
+from multiprocessing import Condition
+from pydoc import Helper
 
 
 HW_SOURCE_FILE = __file__
 
 
-def hailstone(n):
-    """Print the hailstone sequence starting at n and return its
-    length.
+def num_eights(x):
+    """Returns the number of times 8 appears as a digit of x.
 
-    >>> a = hailstone(10)
-    10
-    5
-    16
+    >>> num_eights(3)
+    0
+    >>> num_eights(8)
+    1
+    >>> num_eights(88888888)
     8
-    4
+    >>> num_eights(2638)
+    1
+    >>> num_eights(86380)
     2
-    1
-    >>> a
-    7
-    >>> b = hailstone(1)
-    1
-    >>> b
-    1
+    >>> num_eights(12345)
+    0
+    >>> from construct_check import check
+    >>> # ban all assignment statements
+    >>> check(HW_SOURCE_FILE, 'num_eights',
+    ...       ['Assign', 'AugAssign'])
+    True
     """
     "*** YOUR CODE HERE ***"
+    if x < 10:
+        if x == 8:
+            return 1
+        else:
+            return 0
+    else:
+        if x % 10 == 8:
+            return 1+num_eights(x//10)
+        else:
+            return num_eights(x//10)
 
 
-def product(n, term):
-    """Return the product of the first n terms in a sequence.
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
 
-    n: a positive integer
-    term:  a function that takes one argument to produce the term
-
-    >>> product(3, identity)  # 1 * 2 * 3
+    >>> pingpong(8)
+    8
+    >>> pingpong(10)
     6
-    >>> product(5, identity)  # 1 * 2 * 3 * 4 * 5
-    120
-    >>> product(3, square)    # 1^2 * 2^2 * 3^2
-    36
-    >>> product(5, square)    # 1^2 * 2^2 * 3^2 * 4^2 * 5^2
-    14400
-    >>> product(3, increment) # (1+1) * (2+1) * (3+1)
-    24
-    >>> product(3, triple)    # 1*3 * 2*3 * 3*3
-    162
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    -2
+    >>> pingpong(30)
+    -2
+    >>> pingpong(68)
+    0
+    >>> pingpong(69)
+    -1
+    >>> pingpong(80)
+    0
+    >>> pingpong(81)
+    1
+    >>> pingpong(82)
+    0
+    >>> pingpong(100)
+    -6
+    >>> from construct_check import check
+    >>> # ban assignment statements
+    >>> check(HW_SOURCE_FILE, 'pingpong', ['Assign', 'AugAssign'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+    def helper(i, output, dir):
+        if i == n:
+            return output
+        if num_eights(i) > 0 or i % 8 == 0:
+            return helper(i+1, output-dir, -dir)
+        else:
+            return helper(i+1, output+dir, dir)
+    return helper(1, 1, 1)
+
+
+def missing_digits(n):
+    """Given a number a that is in sorted, increasing order,
+    return the number of missing digits in n. A missing digit is
+    a number between the first and last digit of a that is not in n.
+    >>> missing_digits(1248) # 3, 5, 6, 7
+    4
+    >>> missing_digits(1122) # No missing numbers
+    0
+    >>> missing_digits(123456) # No missing numbers
+    0
+    >>> missing_digits(3558) # 4, 6, 7
+    3
+    >>> missing_digits(35578) # 4, 6
+    2
+    >>> missing_digits(12456) # 3
+    1
+    >>> missing_digits(16789) # 2, 3, 4, 5
+    4
+    >>> missing_digits(19) # 2, 3, 4, 5, 6, 7, 8
+    7
+    >>> missing_digits(4) # No missing numbers between 4 and 4
+    0
+    >>> from construct_check import check
+    >>> # ban while or for loops
+    >>> check(HW_SOURCE_FILE, 'missing_digits', ['While', 'For'])
+    True
     """
     "*** YOUR CODE HERE ***"
 
 
-def accumulate(merger, start, n, term):
-    """Return the result of merging the first n terms in a sequence and start.
-    The terms to be merged are term(1), term(2), ..., term(n). merger is a
-    two-argument commutative function.
-
-    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
-    15
-    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
-    26
-    >>> accumulate(add, 11, 0, identity) # 11
-    11
-    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+def next_largest_coin(coin):
+    """Return the next coin. 
+    >>> next_largest_coin(1)
+    5
+    >>> next_largest_coin(5)
+    10
+    >>> next_largest_coin(10)
     25
-    >>> accumulate(mul, 2, 3, square)    # 2 * 1^2 * 2^2 * 3^2
-    72
-    >>> # 2 + (1^2 + 1) + (2^2 + 1) + (3^2 + 1)
-    >>> accumulate(lambda x, y: x + y + 1, 2, 3, square)
-    19
-    >>> # ((2 * 1^2 * 2) * 2^2 * 2) * 3^2 * 2
-    >>> accumulate(lambda x, y: 2 * x * y, 2, 3, square)
-    576
-    >>> accumulate(lambda x, y: (x + y) % 17, 19, 20, square)
-    16
+    >>> next_largest_coin(2) # Other values return None
+    """
+    if coin == 1:
+        return 5
+    elif coin == 5:
+        return 10
+    elif coin == 10:
+        return 25
+
+
+def count_coins(total):
+    """Return the number of ways to make change for total using coins of value of 1, 5, 10, 25.
+    >>> count_coins(15)
+    6
+    >>> count_coins(10)
+    4
+    >>> count_coins(20)
+    9
+    >>> count_coins(100) # How many ways to make change for a dollar?
+    242
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])                                          
+    True
     """
     "*** YOUR CODE HERE ***"
 
 
-def summation_using_accumulate(n, term):
-    """Returns the sum: term(1) + ... + term(n), using accumulate.
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
 
-    >>> summation_using_accumulate(5, square)
-    55
-    >>> summation_using_accumulate(5, triple)
-    45
-    >>> # You aren't expected to understand the code of this test.
-    >>> # Check that the bodies of the functions are just return statements.
-    >>> # If this errors, make sure you have removed the "***YOUR CODE HERE***".
-    >>> import inspect, ast
-    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
-    ['Expr', 'Return']
+    >>> make_anonymous_factorial()(5)
+    120
+    >>> from construct_check import check
+    >>> # ban any assignments or recursion
+    >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
+    True
     """
-    "*** YOUR CODE HERE ***"
-
-
-def product_using_accumulate(n, term):
-    """Returns the product: term(1) * ... * term(n), using accumulate.
-
-    >>> product_using_accumulate(4, square)
-    576
-    >>> product_using_accumulate(6, triple)
-    524880
-    >>> # You aren't expected to understand the code of this test.
-    >>> # Check that the bodies of the functions are just return statements.
-    >>> # If this errors, make sure you have removed the "***YOUR CODE HERE***".
-    >>> import inspect, ast
-    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
-    ['Expr', 'Return']
-    """
-    "*** YOUR CODE HERE ***"
+    return 'YOUR_EXPRESSION_HERE'
